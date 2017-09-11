d189ede7f645b2915b71f5477f069f4ab2b11ede
Thomas <thomas.flamein@gmail.com>
2017-09-11 18:04:18 +1000

    Landing page

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,14 +1,24 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
 
 class Landing extends Component {
   render() {
     return (
-      <div style={{ textAlign: "center" }}>
-        <h1>Emaily!</h1>
-        Collect feedback from your users
+      <div>
+        <div style={{ textAlign: "center" }}>
+          <h1>Emaily!</h1>
+          <p>Collect feedback from your users</p>
+
+          <a href={this.props.auth ? "/surveys" : "/auth/google"} className="btn-large">Go to my Dashboard</a>
+        </div>
       </div>
-    );
+    );  
   }
 }
 
-export default Landing;
+function mapStateToProps({ auth }) {
+  return { auth };
+}
+
+export default connect(mapStateToProps)(Landing);
